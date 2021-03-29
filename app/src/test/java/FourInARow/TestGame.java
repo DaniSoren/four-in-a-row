26abb666a5a6b584b793a968bd2b6fe26c6c0514
DaniSoren <DaniSoren@hotmail.com>
2021-03-29 17:26:47 +0200

    create test for piece placement

--- app/src/test/java/FourInARow/TestGame.java
+++ app/src/test/java/FourInARow/TestGame.java
@@ -1,6 +1,10 @@
 package FourInARow;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class TestGame {
@@ -9,5 +13,14 @@ public class TestGame {
     @BeforeEach
     public void setUp() {
         game = new StandardGame();
+    }
+
+    @Test
+    void shouldPutBluePieceAtColumn3() {
+        game.putPieceAtColumn(3);
+        PieceType pieceAtColumn3 = game.getPieceAt(5, 3);
+        PieceType blue = PieceType.BLUE;
+        assertThat(pieceAtColumn3, is(notNullValue()));
+        assertThat(pieceAtColumn3, is(blue));
     }
 }
