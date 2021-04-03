56d04738ef07d27bf4604a5f3664d65fed3c4ce0
DaniSoren <DaniSoren@hotmail.com>
2021-04-03 10:48:17 +0200

    enable diagonal test

--- app/src/test/java/FourInARow/TestGame.java
+++ app/src/test/java/FourInARow/TestGame.java
@@ -171,7 +171,6 @@ public class TestGame {
     }
 
     @Test
-    @Disabled
     void shouldHaveLeftDiagonalWinner() {
         Integer[] columns = {2, 3, 3, 4, 4, 5, 4, 5, 6, 5, 5};
         for (Integer column :
